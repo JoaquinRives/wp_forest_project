b837abfd57077d7386f870174267259a0cc81e31
JoaquinRives <joaquin.rives01@gmail.com>
2019-11-30 17:24:31 +0200

    Update __init__.py

--- packages/wp_knn_model/knn_model/__init__.py
+++ packages/wp_knn_model/knn_model/__init__.py
@@ -1,7 +1,6 @@
-import os
 from knn_model.config import config
 
-with open(os.path.join(config.PACKAGE_ROOT, 'VERSION')) as version_file:
+with open(config.PACKAGE_ROOT / 'VERSION') as version_file:
     __version__ = version_file.read().strip()
 
 from knn_model import data_management
