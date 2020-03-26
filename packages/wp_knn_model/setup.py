8d4a7c105a5176e506357a83a5b7884250351ca3
Joaquin Rives Gambin <joaquin.j.rives@utu.fi>
2020-03-26 22:26:21 +0200

    diff test requirements

--- packages/wp_knn_model/setup.py
+++ packages/wp_knn_model/setup.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 
 now = datetime.now()
-dt_string = now.strftime("%d%m%Y-%H%M%S")
+dt_string = now.strftime("%d%m%Y%H%M%S")
 
 # Package meta-data.
 NAME = 'knn_model'
